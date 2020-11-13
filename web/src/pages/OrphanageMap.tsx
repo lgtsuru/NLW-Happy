021ddbfc110b37a040288b71d4fa702402e13522
Luis Gustavo Tsuru <lgtsuru@gmail.com>
2020-11-13 19:03:54 -0300

    added web

---
+++ web/src/pages/OrphanageMap.tsx
@@ -0,0 +1,76 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import {FiArrowRight, FiPlus} from 'react-icons/fi';
+import {Map, TileLayer, Marker, Popup } from 'react-leaflet';
+
+import mapMarkerImg from '../images/map-marker.svg';
+import mapIcon from '../utils/mapIcon';
+import api from '../services/api';
+
+import '../styles/pages/orphanage-map.css';
+
+interface Orphanage {
+	id: number;
+	latitude: number;
+	longitude: number;
+	name: string;
+}
+
+function OrphanageMap () {
+	const [orphanages, setOrphanages] = useState<Orphanage[]>([]);
+
+	useEffect(() => {
+		api.get('orphanages').then(response => {
+			setOrphanages(response.data);
+		});
+	}, []);
+
+	return (
+		<div id="page-map">
+			<aside>
+				<header>
+					<img src={mapMarkerImg} alt="Happy"/>
+
+					<h2>Escolha um orfanato no mapa</h2>
+					<p>Muitas crianças estão esperando a sua visita :)</p>
+				</header>
+
+				<footer>
+					<strong>Curitiba</strong>
+					<span>Paraná</span>
+				</footer>
+			</aside>
+
+			<Map
+				center={[-25.4800227,-49.2135873]}
+				zoom={15}
+				style={{width: '100%', height: '100%'}}
+			>
+				<TileLayer url="https://a.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+				
+				{orphanages.map(orphanage => {
+					return (
+						<Marker 
+							key={orphanage.id}
+							icon={mapIcon}
+							position={[orphanage.latitude, orphanage.longitude]}							
+						>
+							<Popup closeButton={false} minWidth={240} maxWidth={240} className="map-popup">
+								{orphanage.name}
+								<Link to={`/orphanages/${orphanage.id}`}>
+									<FiArrowRight size={20} color="#FFF" />
+								</Link>
+							</Popup>
+						</Marker>			
+					)
+				})}
+			</Map>
+
+			<Link to="/orphanages/create" className="create-orphanage">
+					<FiPlus size={32} color="#FFF" />
+			</Link>
+		</div>
+	);
+}
+
+export default OrphanageMap;
---
+++ web/src/routes.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+import Landing from './pages/Landing';
+import OrphanageMap from './pages/OrphanageMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
+
+function Routes() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={Landing} />
+                <Route path="/App" component={OrphanageMap} />
+                
+                <Route path="/orphanages/create" component={CreateOrphanage} />
+                <Route path="/orphanages/:id" component={Orphanage} />
+            </Switch>
+        </BrowserRouter>
+    );
+}
+
+export default Routes;
